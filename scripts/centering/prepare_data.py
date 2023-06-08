988946db127b234dd4cb019b54e6bbf9a3192c90
Ana Rodrigues <anananacr@gmail.com>
2023-06-08 12:04:44 +0200

    feat: pipeline for generate simulated diffraction patterns

--- scripts/centering/prepare_data.py
+++ scripts/centering/prepare_data.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import h5py
 import fabio
 from typing import List, Optional, Callable, Tuple, Any, Dict
-from generate_artificial import generate_image
+from generate_artificial import generate_image_v2
+import cbf
+
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser(
-        description="Prepare data colelcted to centering with genetic.py and create artificial reference images for centering."
+        description="Generate an simulated data collection for center finding using genetic.py."
     )
     parser.add_argument(
         "-i",
@@ -25,66 +27,64 @@ def main(raw_args=None):
         help="Number of sequential images to create.",
     )
     parser.add_argument(
-        "-r_in",
-        "--r_in",
-        type=int,
-        action="store",
-        help="Size of inner circle in pixels.",
-    )
-    parser.add_argument(
-        "-r_out",
-        "--r_out",
-        type=int,
-        action="store",
-        help="Size of outer circle in pixels.",
-    )
-    parser.add_argument(
         "-o", "--output", type=str, action="store", help="Path to the output hdf5 file."
     )
     args = parser.parse_args(raw_args)
 
     n_images = args.n_images
-    
-    gen_images = []
-    center_pos = []
 
     ## open list file
-    f=open(args.input, 'r')
-    files=f.readlines()
-    total_frames=len(files)
+    f = open(args.input, "r")
+    files = f.readlines()
+    total_frames = len(files)
     ## choose an image from list
-    image_index = np.random.choice(total_frames, n_images)
+    image_index = np.random.choice(total_frames, 1)[0]
 
-    for i in image_index:
-        file_name=files[i][:-1]
-        data=np.array(fabio.open(f"{file_name}").data)
-        gen_images.append(data)
-        ## get theoretical center_pos
-        center_pos.append([835,985])
-
-    ## close list file    
-    f.close()
-
-    f = h5py.File(f"{args.output}/gen_images.h5", "w")
-    f.create_dataset("data", data=gen_images)
-    f.create_dataset("center", data=center_pos)
+    file_name = files[image_index][:-1]
+    data = np.array(fabio.open(f"{file_name}").data)
+    ## close list file
     f.close()
 
     h = data.shape[0]
     w = data.shape[1]
+
+    index_x = np.arange(440, w - 440)
+    index_y = np.arange(440, h - 440)
+
+    center_pos = np.hstack(
+        (
+            np.random.choice(index_x, (n_images, 1)),
+            np.random.choice(index_y, (n_images, 1)),
+        )
+    )
+    center_pos.astype(np.int64)
+    gen_images = []
+
+    f = open("center_list.txt", "w")
+
+    for idx, i in enumerate(center_pos):
+        print(i)
+        data = generate_image_v2(h=h, w=w, center=i)
+        data.astype(int)
+        print(data.dtype)
+        frame = 1
+
+        cbf.write(f"{args.output}/simulated_{idx:03}_001_{frame:05}.cbf", data)
+
+        d = {"crystal": idx, "rot": 1, "center_x": i[0], "center_y": i[1]}
+        f.write("%s\n" % (d))
+
+    f.close()
+
     center_pos = [round(w / 2), round(h / 2)]
     gen_images = []
-    for i in range(n_images):
-        gen_images.append(
-            generate_image(
-                h=h, w=w, r_in=args.r_in, r_out=args.r_out, center=center_pos
-            )
-        )
+    for i in range(1):
+        gen_images.append(generate_image_v2(h=h, w=w))
 
     index = np.arange(0, n_images)
     ref_index = np.random.choice(index, n_images)
 
-    f = h5py.File(f"{args.output}/../gen_images_refs.h5", "w")
+    f = h5py.File(f"{args.output}/gen_images_refs.h5", "w")
     f.create_dataset("data", data=gen_images)
     f.create_dataset("center", data=center_pos)
     f.create_dataset("ref_index", data=ref_index)
