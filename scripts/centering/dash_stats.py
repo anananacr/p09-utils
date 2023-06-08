5a328fdfad959a6b0491be668cea275407095fa7
Ana Rodrigues <anananacr@gmail.com>
2023-06-08 12:19:24 +0200

    feat: read theoretical center positions from txt file

--- scripts/centering/dash_stats.py
+++ scripts/centering/dash_stats.py
@@ -6,6 +6,7 @@ import glob
 import re
 from typing import List, Optional, Callable, Tuple, Any, Dict
 import pandas as pd
+import json
 
 
 def search_files(
@@ -20,48 +21,150 @@ def search_files(
     files = list(
         glob.iglob(rf"{folder_path}/**/{file_name}.{file_format}", recursive=True)
     )
-    files=[x for x in files if x.split('/')[-1]!='gen_images_refs.h5']
-    
+    files = [x for x in files if x.split("/")[-1] != "gen_images_refs.h5"]
+
     if sort == True:
+
         files = sorted(files, key=lambda x: int(x.split("/")[-2][4:]))
-    #print(files)
+    # print(files)
     return files
 
-def table_of_center(crystal: int, rot: int)-> List[int]:
-    data={'crystal': [1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4,5,5],
-        'rot': [1,2,3,4,5,1,2,3,4,5,1,2,3,4,1,2,3,4,1,2],
-        'center_x': [835, 838, 829, 834, 835, 836, 836, 837, 836, 835, 835, 838, 835, 837, 835, 834, 830, 829, 827, 835],
-        'center_y': [985, 974, 971, 965, 955, 920, 919, 917, 886, 877, 869, 844, 825, 816, 802, 787, 777, 766, 758, 753]
-    }
-    df=pd.DataFrame.from_dict(data)
-    match=df.loc[(df['crystal'] == crystal) & (df['rot'] == rot)].reset_index()
-    return [ match['center_x'][0],match['center_y'][0]]
 
-def get_center_theory(files_path: np.ndarray) -> List[int]:
+def table_of_center(
+    crystal: int, rot: int, center_file: str = None, loaded_table_center: Dict = None
+) -> List[int]:
+
+    if loaded_table_center is None:
+        if center_file is None:
+            data = {
+                "crystal": [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5],
+                "rot": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2],
+                "center_x": [
+                    835,
+                    838,
+                    829,
+                    834,
+                    835,
+                    836,
+                    836,
+                    837,
+                    836,
+                    835,
+                    835,
+                    838,
+                    835,
+                    837,
+                    835,
+                    834,
+                    830,
+                    829,
+                    827,
+                    835,
+                ],
+                "center_y": [
+                    985,
+                    974,
+                    971,
+                    965,
+                    955,
+                    920,
+                    919,
+                    917,
+                    886,
+                    877,
+                    869,
+                    844,
+                    825,
+                    816,
+                    802,
+                    787,
+                    777,
+                    766,
+                    758,
+                    753,
+                ],
+            }
+        else:
+            # print(center_file)
+            data = get_table_center(center_file)
+
+            # print(data)
+        loaded_table_center = data.copy()
+
+    data = loaded_table_center
+    df = pd.DataFrame.from_dict(data)
+    # print(df)
+    match = df.loc[(df["crystal"] == crystal) & (df["rot"] == rot)].reset_index()
+
+    return [match["center_x"][0], match["center_y"][0]], loaded_table_center
+
+
+def get_table_center(center_file: str) -> Dict:
+    data = open(center_file, "r").read().splitlines()
+    data = [x.replace("'", '"') for x in data]
+    data = [json.loads(d) for d in data]
+    # print(data)
+    return transpose_dict(data)
+
+
+def transpose_dict(data: list) -> dict:
+    """
+    Transposes a list of dictionaries into a dictionary of lists.
+
+    Parameters:
+        data (list): A list of dictionaries to be transposed.
+
+    Returns:
+        dict: A dictionary with keys from the original dictionaries and values as lists
+              containing the corresponding values from each dictionary.
+
+    Example:
+        >>> data = [{'key1': 1, 'key2': 2}, {'key1': 3, 'key2': 4}]
+        >>> transpose_dict(data)
+        {'key1': [1, 3], 'key2': [2, 4]}
+    """
+    result = {}
+    for d in data:
+        for k, v in d.items():
+            if k not in result:
+                result[k] = []
+            result[k].append(v)
+
+    return result
+
+
+def get_center_theory(
+    files_path: np.ndarray, center_file: np.ndarray, loaded_table_center: str = None
+) -> List[int]:
     center_theory = []
 
     for i in files_path:
-        
+
         label = str(i).split("/")[-1]
-        crystal = int(label.split("_")[0][-2:])
-        rot = int(label.split("_")[1][-3:])
-        #print(crystal, rot)
-        center = table_of_center(crystal, rot)
+        # print(label)
+        # crystal = int(label.split("_")[0][-2:])
+        # rot = int(label.split("_")[1][-3:])
+        crystal = int(label.split("_")[-3][:])
+        rot = int(label.split("_")[-2][:])
+        # print(crystal, rot)
+        center, loaded_table_center = table_of_center(
+            crystal, rot, center_file, loaded_table_center
+        )
         center_theory.append(center)
-    #print(center_theory)
-    center_theory=np.array(center_theory)
-    return center_theory
+    # print(center_theory)
+    center_theory = np.array(center_theory)
+    return center_theory, loaded_table_center
 
 
 def str_to_seconds(time_string: str) -> float:
     # check format
     r = re.compile(".*:.*:.*.*")
-    #print(time_string)
+    # print(time_string)
     if r.match(time_string) is None or len(time_string) > 14:
         raise ValueError("Non expected format time (H:MM:SS.UUUUUU)")
 
     hours = int(time_string[0])
-    #print(hours)
+    # print(hours)
     minutes = int(time_string[2:4])
     seconds = float(time_string[5:])
     return 3600 * hours + 60 * minutes + seconds
@@ -77,34 +180,39 @@ def format_proc_time(time_table: np.ndarray) -> np.ndarray:
     return time_table_in_seconds
 
 
-def get_stats(file_path: str) -> Dict[str, Any]:
+def get_stats(
+    file_path: str, center_file: str = None, loaded_table_center: Dict = None
+) -> Dict[str, Any]:
     f = h5py.File(file_path, "r")
-    try:
-        center_theory = np.array(f["center"])[:]
-    except:
-        center_theory = get_center_theory(np.array(f["id"]))
+    # try:
+    #    center_theory = np.array(f["center"])[:]
+    # except:
+    center_theory, loaded_table_center = get_center_theory(
+        np.array(f["id"]), center_file, loaded_table_center
+    )
+
     center_calc = np.array(f["center_calc"])[:]
-    #ref_image_id = list(np.array(f["ref_index"]))[:]
+    # ref_image_id = list(np.array(f["ref_index"]))[:]
     ref_image_id = np.zeros(len(center_theory))
     param_value = float(np.array(f["param_value_opt"]))
-    param_value = np.ones(len(center_theory))*param_value
-    #print(param_value)
-    size= np.array(f["dimensions"])
+    param_value = np.ones(len(center_theory)) * param_value
+    # print(param_value)
+    size = np.array(f["dimensions"])
 
     processing_time = np.array(f["processing_time"])[:]
     processing_time = format_proc_time(processing_time)
 
-    #err_x_px = abs(center_calc[:, 0] - center_theory[:, 0])
-    #rel_err_x = 100*err_x_px / size[0]
+    # err_x_px = abs(center_calc[:, 0] - center_theory[:, 0])
+    # rel_err_x = 100*err_x_px / size[0]
 
-    #err_y_px = abs(center_calc[:, 1] - center_theory[:, 1])
-    #rel_err_y = 100*err_y_px / size[1]
+    # err_y_px = abs(center_calc[:, 1] - center_theory[:, 1])
+    # rel_err_y = 100*err_y_px / size[1]
 
     err_x_px = center_calc[:, 0] - center_theory[:, 0]
-    rel_err_x = 100*err_x_px / size[0]
+    rel_err_x = 100 * err_x_px / size[0]
 
     err_y_px = center_calc[:, 1] - center_theory[:, 1]
-    rel_err_y = 100*err_y_px / size[1]
+    rel_err_y = 100 * err_y_px / size[1]
     return {
         "err_x_px": list(err_x_px),
         "err_y_px": list(err_y_px),
@@ -113,7 +221,7 @@ def get_stats(file_path: str) -> Dict[str, Any]:
         "processing_time": processing_time,
         "ref_id": list(ref_image_id),
         "param_value": list(param_value),
-    }
+    }, loaded_table_center
 
 
 def detect_outliers(
@@ -129,8 +237,8 @@ def detect_outliers(
             non_outliers_x = df.err_x_px.between(lim_x[0], lim_x[1])
             non_outliers_y = df.err_y_px.between(lim_y[0], lim_y[1])
         else:
-            lim_x = [-1*cut_px, cut_px]
-            lim_y = [-1*cut_px, cut_px]
+            lim_x = [-1 * cut_px, cut_px]
+            lim_y = [-1 * cut_px, cut_px]
             non_outliers_x = df.err_x_px.between(lim_x[0], lim_x[1])
             non_outliers_y = df.err_y_px.between(lim_y[0], lim_y[1])
     else:
@@ -142,8 +250,8 @@ def detect_outliers(
             non_outliers_x = df.rel_err_x.between(lim_x[0], lim_x[1])
             non_outliers_y = df.rel_err_y.between(lim_y[0], lim_y[1])
         else:
-            lim_x = [-1*cut_percent, cut_percent]
-            lim_y = [-1*cut_percent, cut_percent]
+            lim_x = [-1 * cut_percent, cut_percent]
+            lim_y = [-1 * cut_percent, cut_percent]
             non_outliers_x = df.rel_err_x.between(lim_x[0], lim_x[1])
             non_outliers_y = df.rel_err_y.between(lim_y[0], lim_y[1])
     outliers_x = ~non_outliers_x
@@ -204,7 +312,7 @@ def generate_individual_plots(
         axes[0, 1].scatter(index_outliers_x, err, marker="x", color="r", s=100)
 
         index_outliers_y = df.index[outliers[1]]
-        
+
         err = df.rel_err_y[outliers[1]]
         axes[0, 1].scatter(index_outliers_y, err, marker="x", color="r", s=100)
 
@@ -228,7 +336,7 @@ def generate_individual_plots(
 
 
 def generate_individual_hists(df: pd.DataFrame, title: str = None):
-    bins=np.arange(-5.5,5.5,0.5)
+    bins = np.arange(-5.5, 5.5, 0.5)
     fig, axes = plt.subplots(2, 2)
 
     df.hist(
@@ -250,7 +358,6 @@ def generate_individual_hists(df: pd.DataFrame, title: str = None):
         stacked=False,
         color="gray",
         alpha=0.8,
-        
     )
 
     df.hist(
@@ -264,7 +371,7 @@ def generate_individual_hists(df: pd.DataFrame, title: str = None):
         alpha=0.7,
         rwidth=0.5,
         width=0.5,
-        bins=bins
+        bins=bins,
     )
     df.hist(
         column=["rel_err_x"],
@@ -277,10 +384,10 @@ def generate_individual_hists(df: pd.DataFrame, title: str = None):
         alpha=0.7,
         rwidth=0.5,
         width=0.5,
-        bins=bins
-        )
+        bins=bins,
+    )
     fig.legend()
-    bins=np.arange(-80,80,4)
+    bins = np.arange(-80, 80, 4)
     df.hist(
         column=["err_y_px"],
         figsize=(10, 10),
@@ -293,7 +400,7 @@ def generate_individual_hists(df: pd.DataFrame, title: str = None):
         alpha=0.7,
         rwidth=0.5,
         width=4,
-        bins=bins
+        bins=bins,
     )
     df.hist(
         column=["err_x_px"],
@@ -307,22 +414,26 @@ def generate_individual_hists(df: pd.DataFrame, title: str = None):
         alpha=0.7,
         rwidth=0.5,
         width=4,
-        bins=bins
+        bins=bins,
     )
     plt.legend()
     plt.show()
 
-def mergeDictionary(dict_1: Dict[str, float], dict_2: Dict[str, float]) -> Dict[str, float]:
+
+def mergeDictionary(
+    dict_1: Dict[str, float], dict_2: Dict[str, float]
+) -> Dict[str, float]:
     merged_dict = {**dict_1, **dict_2}
-    #print(len(merged_dict['err_x_px']))
-    new_list=[]
+    # print(len(merged_dict['err_x_px']))
+    new_list = []
     for key, list_of_values in merged_dict.items():
         if key in dict_1 and key in dict_2 and isinstance(dict_1[key], list):
             for x in dict_1[key]:
                 list_of_values.append(x)
-        merged_dict[key]=list_of_values
-    #print(len(merged_dict['err_x_px']))
+        merged_dict[key] = list_of_values
+    # print(len(merged_dict['err_x_px']))
     return merged_dict
+
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser(
@@ -343,17 +454,19 @@ def main(raw_args=None):
         folder_path, file_name="gen_images*", file_format="h5", sort=True
     )
 
-    merged_stats={"err_x_px": [],
+    merged_stats = {
+        "err_x_px": [],
         "err_y_px": [],
         "rel_err_x": [],
         "rel_err_y": [],
         "processing_time": [],
         "ref_id": [],
-        "param_value": []}
+        "param_value": [],
+    }
 
     for i in files:
         stats = get_stats(i)
-        merged_stats=mergeDictionary(stats, merged_stats)
+        merged_stats = mergeDictionary(stats, merged_stats)
 
     df_stats = pd.DataFrame.from_dict(data=merged_stats)
     print(df_stats.param_value[0])
@@ -363,6 +476,7 @@ def main(raw_args=None):
     outliers = detect_outliers(df_stats, cut_percent=2, mean=False)
     generate_individual_plots(df_stats, title, outliers)
     generate_individual_hists(df_stats, title)
+
 
 if __name__ == "__main__":
     main()
