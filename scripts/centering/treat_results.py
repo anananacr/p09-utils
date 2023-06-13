35743f4acb397986dbd43143841bc976df98ff14
Ana Rodrigues <anananacr@gmail.com>
2023-06-13 13:23:00 +0200

    fix: nan cols problem solved

--- scripts/centering/treat_results.py
+++ scripts/centering/treat_results.py
@@ -26,7 +26,9 @@ def main(raw_args=None):
 
     args = parser.parse_args(raw_args)
     file_path = f"{args.input}"
-    df = pd.read_csv(file_path, usecols=["rel_err_x", "rel_err_y", "param_value"])
+    df = pd.read_csv(
+        file_path, usecols=["rel_err_x", "rel_err_y", "processing_time", "param_value"]
+    )
 
     df.dropna(inplace=True)
     df.reset_index(drop=True, inplace=True)
@@ -37,34 +39,40 @@ def main(raw_args=None):
     file_path = f"{args.param}"
     df_id = pd.read_csv(file_path)
 
-    merged_df = pd.DataFrame({"g": [], "r": [], "t": [], "l": []})
+    merged_df = pd.DataFrame({"g": [], " r": [], " t": [], " l": []})
     for i in df.param_value[:]:
         param = df_id.iloc[[int(i)], [0, 1, 2, 3]]
         merged_df = pd.concat([merged_df, param], ignore_index=True)
-
     final_df = pd.concat([df, merged_df], axis=1, ignore_index=False)
+
     x = np.array(final_df.rel_err_x)
     y = np.array(final_df.rel_err_y)
-    z = np.array(final_df.g)
+    z = np.array(final_df.processing_time, dtype=np.int32)
     param_value = np.array(final_df.param_value)
+    param_value_g = np.array(final_df.g)
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
     ax.scatter(x, y, z, marker="o")
 
-    ax.set_xlabel("rel_err_x [%]")
-    ax.set_ylabel("rel_err_y [%]")
-    ax.set_zlabel("Generations")
-
-    selected_index = np.where((x < 0.05) & (y < 0.05))
+    ax.set_xlabel("rel_err_x (%)")
+    ax.set_ylabel("rel_err_y (%)")
+    ax.set_zlabel("Processing time (s)")
+    ax.set_zlim(0, 3600)
+    selected_index = np.where((x < 0.06) & (y < 0.06))
     x_cut = x[selected_index]
     y_cut = y[selected_index]
     z_cut = z[selected_index]
     id_cut = param_value[selected_index]
+
+    selected_index_2 = np.where((x < 0.06) & (y < 0.06) & (z < 1000))
     print(id_cut)
+
+    gen_cut = param_value_g[selected_index_2]
+    print(gen_cut)
     ax.scatter(x_cut, y_cut, z_cut, marker="o", color="red")
 
     plt.show()
-    print(final_df.head)
+    
 
 
 if __name__ == "__main__":
