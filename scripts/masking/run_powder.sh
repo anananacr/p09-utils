ac609d5e71aa6dc86c384660d510e027b21aa923
Ana Rodrigues <anananacr@gmail.com>
2023-07-04 17:34:23 +0200

    feat: virtual powder image from the sum of images

---
+++ scripts/masking/run_powder.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_01_001.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_01_01.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_01_002.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_01_02.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_01_003.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_01_03.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_01_004.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_01_04.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_01_005.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_01_05.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_02_001.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_02_01.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_02_002.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_02_02.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_02_003.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_02_03.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_02_004.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_02_04.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_02_005.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_02_05.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_03_001.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_03_01.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_03_002.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_03_02.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_03_003.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_03_03.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_03_004.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_03_04.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_04_001.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_04_01.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_04_002.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_04_02.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_04_003.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_04_03.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_04_004.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_04_04.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_05_001.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_05_01.h5;
+
+python3 powder_to_mask.py -i /gpfs/cfel/user/rodria/proc/p09_test/moving_beam/lists/lyso_05_002.lst -n 100 -o /gpfs/cfel/user/rodria/proc/p09_test/mov_lyso_0/for_mask/lyso_05_02.h5;
